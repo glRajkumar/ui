0553eeb1ed7e9a9973007cdba2af862265fa9449
Raj kumar <glrajv369@gmail.com>
2025-11-12 16:07:32 +0530

    test: mdx content and fumadocs ui

--- lib/source.ts
+++ lib/source.ts
@@ -1,7 +1,10 @@
-import { docs } from '@/.source';
+import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 import { loader } from 'fumadocs-core/source';
+
+import { docs } from '@/.source';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  plugins: [lucideIconsPlugin()],
 })
