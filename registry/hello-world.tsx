f401c6f81f360d09bde3c72339c861f2f07a1bc7
Raj kumar <glrajv369@gmail.com>
2025-11-12 11:55:56 +0530

    feat: shadcn regitry setup

---
+++ registry/hello-world.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export function HelloWorld() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Hello World</h1>
+      <Button>Test</Button>
+    </div>
+  )
+}
