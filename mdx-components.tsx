5e86f8c94e484f50580682f3cd359d565a1f6cad
Raj kumar <glrajv369@gmail.com>
2025-11-13 13:32:14 +0530

    add: AutoTypeTable support

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,16 @@
+import type { MDXComponents } from 'mdx/types';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
-import type { MDXComponents } from 'mdx/types';
+import { createGenerator } from 'fumadocs-typescript';
+import { AutoTypeTable } from 'fumadocs-typescript/ui';
+
+const generator = createGenerator()
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    AutoTypeTable: props => <AutoTypeTable {...props} generator={generator} />,
     ...components,
   };
 }
