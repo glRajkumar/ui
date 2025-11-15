1c040bafdc87d96d1a0fe280ff8cc8f1ea6181da
Raj kumar <glrajv369@gmail.com>
2025-11-15 15:34:36 +0530

    feat: alert-dialog docs

--- components/examples/alert-dialog.tsx
+++ components/examples/alert-dialog.tsx
@@ -3,12 +3,30 @@
 import { useState } from "react";
 
 import { AlertDialogWrapper } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 export function AlertDialogExample() {
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <AlertDialogWrapper
-        trigger="Alert"
+        trigger={<Button variant="outline" size="sm">Delete</Button>}
+      />
+
+      <AlertDialogWrapper
+        open={open}
+        onOpenChange={setOpen}
+        trigger={<Button variant="outline" size="sm">Controlled</Button>}
+        title="Do you want to leave page"
+        description=""
+        actionText="Proceed"
+        onConfirm={() => setOpen(p => !p)}
+      />
+
+      <AlertDialogWrapper
+        trigger={<Button variant="outline" size="sm">No Action</Button>}
+        actionText=""
       />
     </>
   )
