c67826e5e23a7d2668b9bb8314b5cf3148a8c156
Raj kumar <glrajv369@gmail.com>
2026-03-20 22:24:03 +0530

    update: tabs base-ui

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -79,4 +79,65 @@ function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
   )
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent, tabsListVariants }
+type tabItemT = {
+  value: string
+  trigger: React.ReactNode
+  content: React.ReactNode
+  disabled?: boolean
+  triggerCls?: string
+  contentCls?: string
+}
+
+type tabItemsT = tabItemT[]
+type tabsWrapperProps = {
+  tabs: tabItemsT
+  listCls?: string
+  triggerCls?: string
+  contentCls?: string
+}
+
+function TabsWrapper({
+  tabs,
+  listCls,
+  triggerCls,
+  contentCls,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Root> & tabsWrapperProps) {
+  return (
+    <Tabs {...props}>
+      <TabsList className={listCls}>
+        {tabs.map(tab => (
+          <TabsTrigger
+            key={tab.value}
+            value={tab.value}
+            disabled={tab.disabled}
+            className={cn(triggerCls, tab.triggerCls)}
+          >
+            {tab.trigger}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+
+      {tabs.map(tab => (
+        <TabsContent
+          key={tab.value}
+          value={tab.value}
+          className={cn(contentCls, tab.contentCls)}
+        >
+          {tab.content}
+        </TabsContent>
+      ))}
+    </Tabs>
+  )
+}
+
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+  tabsListVariants,
+  TabsWrapper,
+  type tabItemT,
+  type tabItemsT,
+}
